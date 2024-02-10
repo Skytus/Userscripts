e84ecf6a823adcdc447a0116b9e0407a0df7f331
Skytus <41904028+Skytus@users.noreply.github.com>
2024-02-10 19:32:59 +0000

    Create piped.user.js

---
+++ piped.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            Redirect YouTube to Piped
+// @version         1.0.0
+// @description     Redirect YouTube to Piped
+// @author          stef312
+// @namespace       stef312
+// @grant           none
+// @match           https://*.youtube.com/*
+// @match           https://*.youtu.be/*
+// @run-at          document-start
+// ==/UserScript==
+
+(function() {
+    var url = new URL(window.location);
+
+    if (url.hostname === "www.youtube.com" && url.pathname === "/watch") {
+        const videoId = url.searchParams.get("v");
+
+        if (videoId) {
+            const newUrl = `https://piped.video/watch?v=${videoId}`;
+            window.location = newUrl;
+        }
+    }
+})();
